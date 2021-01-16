66bb69a5e3a779303bb24c68943c23ad9114f8e6
Naomi Carvalho <naomicarv@gmail.com>
2021-01-16 11:44:53 -0800

    Init project commit

---
+++ Stimm/Stimm/StimmApp.swift
@@ -0,0 +1,17 @@
+//
+//  StimmApp.swift
+//  Stimm
+//
+//  Created by Samantha Carvalho on 1/16/21.
+//
+
+import SwiftUI
+
+@main
+struct StimmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
